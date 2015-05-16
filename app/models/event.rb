15a752efd29d5c502720ec23597752b7a935a594
munyite-megzs <margaret.sitati1@gmail.com>
2015-05-16 08:48:38 +0300

    added status

--- app/models/event.rb
+++ app/models/event.rb
@@ -20,8 +20,18 @@ class Event < ActiveRecord::Base
 
 		def all_tags=(names)
              self.tags = names.split(",").map do |t|
+             	#searches the db to se whether it is there if not it adds it
              Tag.where(name: t.strip).first_or_create!
              end
          end
+
+
+         def self.tagged_with(name)
+			 Tag.find_by_name!(name).events
+		end
+
+		def self.tag_counts
+              Tag.select("tags.name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id")
+        end
 
 end
