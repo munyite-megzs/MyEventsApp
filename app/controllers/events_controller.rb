4406c71767ab949d41cf86748bc698446068fd0f
munyite-megzs <margaret.sitati1@gmail.com>
2015-05-16 07:34:50 +0300

    added tags to events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -61,25 +61,39 @@ class EventsController < ApplicationController
       format.json { head :no_content }
     end
   end
+            
+      
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_event
-      @event = Event.friendly.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def event_params
-      params.require(:event).permit(:title, :start_date, :end_date, :location, :agenda, :address, :organizer_id, :created_at, :updated_at)
-    end
+      private
+          
 
 
-def event_owner!
-authenticate_user!
-if @event.organizer_id != current_user.id
-redirect_to events_path
-flash[:notice] = 'You do not have enough
-permissions to do this'
+
+
+  
+        # Use callbacks to share common setup or constraints between actions.
+        def set_event
+          @event = Event.friendly.find(params[:id])
+        end
+
+        # Never trust parameters from the scary internet, only allow the white list through.
+        def event_params
+          params.require(:event).permit(:title, :start_date, :end_date, :location, :agenda, :address, :organizer_id, :created_at, :updated_at)
+        end
+
+
+        
+
+
+
+      def event_owner!
+            authenticate_user!
+            if @event.organizer_id != current_user.id
+            redirect_to events_path
+            flash[:notice] = 'You do not have enough
+            permissions to do this'
+           end
+       end
 end
-end
-end
+
+
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,13 +1,27 @@
 class Event < ActiveRecord::Base
+		     extend FriendlyId
+		     friendly_id :slug_candidates, use: :slugged
+		 
+			 belongs_to :organizers, class_name: "User"
+			 has_many :taggings
+			 has_many :tags, through: :taggings
 
-	belongs_to :organizers, class_name: "User"
+		 def slug_candidates
+				[
+				:title,
+				[:title, :location],
+				]
+		end
 
-	extend FriendlyId
-	friendly_id :slug_candidates, use: :slugged
-def slug_candidates
-[
-:title,
-[:title, :location],
-]
-end
+
+		def all_tags
+			 tags.map(&:name).join(", ")
+		end
+
+		def all_tags=(names)
+             self.tags = names.split(",").map do |t|
+             Tag.where(name: t.strip).first_or_create!
+             end
+         end
+
 end
